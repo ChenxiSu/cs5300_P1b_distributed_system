2bb404d9327d5fd01a88d1f7deb6bed87ace4983
Yuzhuo Sun <Sun@dhcp-rhodes-3033.redrover.cornell.edu>
2016-04-07 21:51:03 -0400

    re-orgnized

--- launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-AMI=ami-c229c0a2 
-S3_BUCKET=cornell-cs2238-cs5300
-
-aws s3 cp project1a.war s3://${S3_BUCKET}/project1a.war
-aws ec2 run-instances --image-id ${AMI} --count 5 --instance-type t2.micro -f s3://${S3_BUCKET}/install-my-app.sh
